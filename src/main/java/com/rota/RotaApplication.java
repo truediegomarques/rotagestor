8f944f5311fe753940d594acc169f67cfa4a7a5b
truediegomarques <jdiegomarquesr@gmail.com>
2019-12-10 22:02:21 -0300

    Remoção temporária de Manipulação de Arquivo Externo

--- src/main/java/com/rota/RotaApplication.java
+++ src/main/java/com/rota/RotaApplication.java
@@ -3,21 +3,12 @@ package com.rota;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.rota.config.GeraArquivo;
-
 @SpringBootApplication
 public class RotaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RotaApplication.class, args);
 		
-		GeraArquivo ga = new GeraArquivo();
-		ga.criaDiretorio("/home/diego/Público/tutorial2");
-		ga.criaArquivo("tutorial22.txt");
-
-		ga.listaArquivos();
-	
-
 		
 	}
 
