79fd49448f7291139a685e125bcba50099d19d4a
diego <diego@10.0.0.108>
2021-02-09 13:51:05 -0300

    Inicial

---
+++ rotagestor/src/main/java/com/sds/rotagestor/RotagestorApplication.java
@@ -0,0 +1,13 @@
+package com.sds.rotagestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotagestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotagestorApplication.class, args);
+	}
+
+}
---
+++ rotagestor/src/test/java/com/sds/rotagestor/RotagestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.rotagestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotagestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
