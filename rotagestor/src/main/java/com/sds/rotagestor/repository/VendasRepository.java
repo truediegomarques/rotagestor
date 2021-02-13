31d08d6ebd716a839276b925f303a3bbbcdb4530
diego <diego@10.0.0.108>
2021-02-12 23:07:17 -0300

    opa2

---
+++ rotagestor/src/main/java/com/sds/rotagestor/repository/VendasRepository.java
@@ -0,0 +1,9 @@
+package com.sds.rotagestor.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sds.rotagestor.domain.Venda;
+
+public interface VendasRepository extends JpaRepository<Venda, Integer> {
+
+}
