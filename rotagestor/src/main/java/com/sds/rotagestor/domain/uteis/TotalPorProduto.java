2de824ea1e0f684e582c7dbfcb53cd3e4fa21e83
Diego Marques <jdiegomarques@gmail.com>
2021-02-23 12:04:32 -0300

    Bean Total

---
+++ rotagestor/src/main/java/com/sds/rotagestor/domain/uteis/TotalPorProduto.java
@@ -0,0 +1,34 @@
+package com.sds.rotagestor.domain.uteis;
+
+import java.math.BigDecimal;
+
+public class TotalPorProduto {
+    private Integer id;
+    private BigDecimal total;
+
+    public TotalPorProduto(Integer id, BigDecimal total) {
+        this.id = id;
+        this.total = total;
+    }
+
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+   
+
+    
+}
