d4e09ea87d9160f4334b0da0505d9504fe09a1fc
Diego Marques <jdiegomarques@gmail.com>
2021-02-23 22:56:28 -0300

    Fudeu

--- rotagestor/src/main/java/com/sds/rotagestor/repository/ItensvendasRepository.java
+++ rotagestor/src/main/java/com/sds/rotagestor/repository/ItensvendasRepository.java
@@ -17,7 +17,8 @@ public interface ItensvendasRepository extends JpaRepository<Itensvenda, Integer
     @Query(value = "SELECT p FROM Itensvenda p WHERE p.datamov BETWEEN :dtinicio AND :dtfim")
     List<Itensvenda> periodo(@Param("dtinicio") Date dtinicio, @Param("dtfim") Date dtfim);
 	
-    @Query(value="SELECT  new com.sds.rotagestor.domain.uteis.TotalPorProduto(p.idProduto,SUM(p.valor))  FROM  Itensvenda p WHERE  p.datamov  BETWEEN :dtinicio AND :dtfim GROUP BY p.id",
+    @Query(value="SELECT  new com.sds.rotagestor.domain.uteis.TotalPorProduto(p.idProduto,SUM(p.valor)) FROM  Itensvenda p " +
+    "WHERE  p.datamov  BETWEEN :dtinicio AND :dtfim GROUP BY p.idProduto",
      nativeQuery = false)
     List<TotalPorProduto> periodoTotalProduto(@Param("dtinicio") Date dtinicio, @Param("dtfim") Date dtfim);
 
