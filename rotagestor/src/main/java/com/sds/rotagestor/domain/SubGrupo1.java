213ad180de8519f5b5bae9e319511b95f282cfb3
Diego Marques <jdiegomarques@gmail.com>
2021-03-16 23:43:32 -0300

    Atual

--- rotagestor/src/main/java/com/sds/rotagestor/domain/SubGrupo1.java
+++ rotagestor/src/main/java/com/sds/rotagestor/domain/SubGrupo1.java
@@ -11,7 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
