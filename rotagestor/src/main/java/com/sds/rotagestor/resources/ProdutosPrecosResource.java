328bb08bb3fae172031cef0f9ef723e3aa11cd58
Diego Marques <jdiegomarques@gmail.com>
2021-04-06 22:53:37 -0300

    Só limpeza de importações

--- rotagestor/src/main/java/com/sds/rotagestor/resources/ProdutosPrecosResource.java
+++ rotagestor/src/main/java/com/sds/rotagestor/resources/ProdutosPrecosResource.java
@@ -4,14 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import com.sds.rotagestor.domain.ProdutoPreco;
-import com.sds.rotagestor.domain.uteis.ProdutoEstoqueEan;
 import com.sds.rotagestor.repository.ProdutosPrecosRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
